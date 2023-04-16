cbf0ee6353abbd3f1de9ab07de4793a72c9ece27
Noha Nabil <46314500+nohaelgendy@users.noreply.github.com>
2023-04-16 20:36:32 +0200

    generate pdf file with input text

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 import os
-
 from flask import Flask, redirect, render_template, request, url_for
+from fpdf import FPDF
 
 app = Flask(__name__)
 
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        return render_template("index.html", data = "data")
+        # Get the text input from the form
+        text_input = request.form['text_input']
+        
+        # Generate a PDF file with the input text
+        pdf = FPDF()
+        pdf.add_page()
+        pdf.set_font("Arial", size=12)
+        pdf.cell(200, 10, txt=text_input, ln=1)
+        pdf.output("static/output.pdf")
+        
+        # Return a response to the user with a link to download the PDF file
+        return f'<a href="/download">Download PDF</a>'
+        
+    # If the request method is GET, show the form to the user
+    return render_template('index.html')
 
-    return render_template("index.html")
+@app.route('/download')
+def download():
+    # Return the PDF file to the user for download
+    return app.send_static_file('output.pdf')

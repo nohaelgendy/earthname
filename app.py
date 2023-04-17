df0e0343533d9c171fb9df9acf3ffffc10878bed
Noha Nabil <46314500+nohaelgendy@users.noreply.github.com>
2023-04-17 13:11:23 +0200

    change prompot and pdf page layout

--- app.py
+++ app.py
@@ -10,29 +10,37 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 def index():
     if request.method == "POST":
         # Get the text input from the form
-        text_input = request.form['text_input']
+        name = request.form['text_input']
+        if not name: name = "Muhammed"
+
+        title = 'Earth & '+ name
 
         #
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=generate_prompt(),
-            temperature=0.6,
-        )
+            prompt=generate_prompt(name),
+            max_tokens=1000,
+            n=1,
+            stream=False,
+            stop=None,
+            temperature=0.5,)
+
         result=response.choices[0].text
-        print("response: -----> ",response)
-        print("result: -----> ",result)
         
         # Generate a PDF file with the input text
         pdf = FPDF()
         pdf.add_page()
-        pdf.set_font("Arial", size=12)
-        pdf.cell(200, 10, txt=text_input, ln=1)
 
+        pdf.set_font("Arial", size=20, style="B")
+        pdf.cell(0, 10, title, ln=1, align="C")
+
+        pdf.set_font("Arial", size=14)
+        pdf.multi_cell(0, 10, result, 0, 'L')
+        print(result)
         # Add an image to the PDF
-        pdf.image('static/img/earth.jpeg', x=pdf.w / 2 - 50, y=pdf.h / 2 - 50, w=150, h=150)
-        pdf.cell(40, 10, result)
-    
-        pdf.output("static/output.pdf")
+        pdf.image('static/img/earth.jpeg', x=10, y=140, w=200)
+        
+        pdf.output("static/earth.pdf")
         
         # Return a response to the user with a link to download the PDF file
         return f'<a href="/download">Download PDF</a>'
@@ -43,13 +51,9 @@ def index():
 @app.route('/download')
 def download():
     # Return the PDF file to the user for download
-    return app.send_static_file('output.pdf')
+    return app.send_static_file('earth.pdf')
 
-def generate_prompt():
-    return """Can you provide fun and educational facts in 10 words
-     about planet Earth that would be suitable for kids to learn?
-    The information should be easy to understand and engaging,
-    and cover topics such as the planet's size, environment,
-    and unique features. Please also include any interesting
-    facts about Earth's place in the solar system and the 
-    importance of taking care of our planet."""
+def generate_prompt(name):
+    return """generate story in two paragraph about planet 
+    earth and how to save  earth suitable to kids by character
+    input kid name :{} """.format(name.capitalize())

5c20927fe51eb76e0e73ec2dc658f34bfec3c63f
Noha Nabil <46314500+nohaelgendy@users.noreply.github.com>
2023-04-17 13:38:19 +0200

    refactor code

--- app.py
+++ app.py
@@ -6,54 +6,53 @@ import openai
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-@app.route("/", methods=("GET", "POST"))
+@app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        # Get the text input from the form
         name = request.form['text_input']
         if not name: name = "Muhammed"
+        title = f"Earth & {name}"
+        result = generate_story(name)
 
-        title = 'Earth & '+ name
-
-        #
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(name),
-            max_tokens=1000,
-            n=1,
-            stream=False,
-            stop=None,
-            temperature=0.5,)
-
-        result=response.choices[0].text
-        
-        # Generate a PDF file with the input text
-        pdf = FPDF()
-        pdf.add_page()
-
-        pdf.set_font("Arial", size=20, style="B")
-        pdf.cell(0, 10, title, ln=1, align="C")
-
-        pdf.set_font("Arial", size=14)
-        pdf.multi_cell(0, 10, result, 0, 'L')
-        print(result)
-        # Add an image to the PDF
-        pdf.image('static/img/earth.jpeg', x=10, y=140, w=200)
-        
+        pdf = create_pdf(title, result)
         pdf.output("static/earth.pdf")
         
-        # Return a response to the user with a link to download the PDF file
-        return f'<a href="/download">Download PDF</a>'
-        
-    # If the request method is GET, show the form to the user
-    return render_template('index.html')
+        return app.send_static_file("earth.pdf")
+    
+    return render_template("index.html")
 
-@app.route('/download')
-def download():
-    # Return the PDF file to the user for download
-    return app.send_static_file('earth.pdf')
+# @app.route("/download")
+# def download():
+#     return app.send_static_file("earth.pdf")
 
-def generate_prompt(name):
-    return """generate story in two paragraph about planet 
-    earth and how to save  earth suitable to kids by character
-    input kid name :{} """.format(name.capitalize())
+def generate_story(name):
+    prompt = f"""generate story in two paragraph about planet earth and how to save earth 
+                suitable to kids by character input kid name: {name.capitalize()}"""
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        max_tokens=1000,
+        n=1,
+        stream=False,
+        stop=None,
+        temperature=0.5,
+    )
+    return response.choices[0].text
+
+def create_pdf(title, result):
+    pdf = FPDF()
+    pdf.add_page()
+
+    pdf.set_font("Arial", size=20, style="B")
+    pdf.cell(0, 10, title, ln=1, align="C")
+
+    pdf.set_font("Arial", size=14)
+    pdf.multi_cell(0, 10, result, 0, "L")
+
+    pdf.image("static/img/earth.jpeg", x=10, y=140, w=200)
+
+    pdf.set_y(pdf.h-35)
+    pdf.set_font("Arial", size=8)
+    pdf.cell(0, 10, "earthname.com", 0, 0, "C")
+
+    return pdf
